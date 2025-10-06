a377ae6e973168919a9bed0ccb49da7fec151fc0
Hidequel Puga <codefivestar@gmail.com>
2025-10-05 23:08:06 -0500

    Add .env file, for example purpose

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+//const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
+
 
 // Middleware para parsear JSON
 app.use(express.json());
+app.use(cors());
 
 // Datos de ejemplo
 let usuarios = [
